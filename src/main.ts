3c1ffbfeb560ef1691d375f399e217aabbebea84
import-username <stermanv2@gmail.com>
2022-01-27 22:46:43 -0500

    added cors middleware

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT: string = process.env.PORT || "3001";
 
 const app = express();
 
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }))
 
-
-app.listen(() => {
+app.listen(PORT, () => {
     console.log(`Server started, listening on port ${PORT}.`);
 });
